91a4b186ad11f9d48b996e7dbbc8dc3446294c9a
Netvon <tomvannimwegen@gmail.com>
2016-06-09 19:18:04 +0200

    Added basic sound support

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,12 @@ import SetupGame from './model/games/SetupGame';
 import Persistence from './util/Persistence';
 import Gameboard from './model/board/Gameboard';
 import Cell from './model/Cell';
+import SoundFXViewModel from './viewmodel/SoundFXViewModel';
 
 (function () {
+
+    let fx = new SoundFXViewModel('audio/test.mp3');
+    fx.play();
 
     let allShips = [];
 
@@ -22,41 +26,41 @@ import Cell from './model/Cell';
 
     UserViewModel.getCurrent(battleshipApi, user => user.displayOn('#user-info'));
 
-    SetupGame.deleteAll(battleshipApi, e => {
-
-        Ship.getAll(battleshipApi, ships => {
-
-            allShips = ships;
-
-            ships.forEach(ship => Hu.queryAppend('ul#ship-list', `<li>${ship.name}</li>`));
-
-            SetupGame.create(battleshipApi, game => {
-                console.log(`Created new game: ${game.id}`);
-                UserViewModel.getGames(battleshipApi, games => {
-                    games.forEach(g => Hu.queryAppend('#all-games > ul', `<li id="g-${g.id}">${g.id} - ${g.state}</li>`));
-                });
-
-                let gameboard = new Gameboard();
-                gameboard.placeShip(allShips[0], new Cell(1, 1), 'vertical');
-                gameboard.placeShip(allShips[1], new Cell(2, 1), 'vertical');
-                gameboard.placeShip(allShips[2], new Cell(3, 1), 'vertical');
-                gameboard.placeShip(allShips[3], new Cell(4, 1), 'vertical');
-                gameboard.placeShip(allShips[4], new Cell(5, 1), 'vertical');
-
-                game.submitGameboard(battleshipApi, gameboard, data => {
-                    console.log("Submitted Gameboard");
-                    console.log(data);
-
-                    Hu.querySet(`#g-${game.id}`, `${game.id} - ${game.state}`);
-
-                    data.doShot(battleshipApi, new Cell(1, 1), data => console.log(`Shot output: ${data}`));
-                });
-
-            }, true);
-        });
-
-
-    });
+    // SetupGame.deleteAll(battleshipApi, e => {
+    //
+    //     Ship.getAll(battleshipApi, ships => {
+    //
+    //         allShips = ships;
+    //
+    //         ships.forEach(ship => Hu.queryAppend('ul#ship-list', `<li>${ship.name}</li>`));
+    //
+    //         SetupGame.create(battleshipApi, game => {
+    //             console.log(`Created new game: ${game.id}`);
+    //             UserViewModel.getGames(battleshipApi, games => {
+    //                 games.forEach(g => Hu.queryAppend('#all-games > ul', `<li id="g-${g.id}">${g.id} - ${g.state}</li>`));
+    //             });
+    //
+    //             let gameboard = new Gameboard();
+    //             gameboard.placeShip(allShips[0], new Cell(1, 1), 'vertical');
+    //             gameboard.placeShip(allShips[1], new Cell(2, 1), 'vertical');
+    //             gameboard.placeShip(allShips[2], new Cell(3, 1), 'vertical');
+    //             gameboard.placeShip(allShips[3], new Cell(4, 1), 'vertical');
+    //             gameboard.placeShip(allShips[4], new Cell(5, 1), 'vertical');
+    //
+    //             game.submitGameboard(battleshipApi, gameboard, data => {
+    //                 console.log("Submitted Gameboard");
+    //                 console.log(data);
+    //
+    //                 Hu.querySet(`#g-${game.id}`, `${game.id} - ${game.state}`);
+    //
+    //                 data.doShot(battleshipApi, new Cell(1, 1), data => console.log(`Shot output: ${data}`));
+    //             });
+    //
+    //         }, true);
+    //     });
+    //
+    //
+    // });
 }());
 
 
